d687e8d430d98d11a1b768998c36f76fb24c1620
codistmonk <codistmonk@users.noreply.github.com>
2015-09-12 10:34:53 +0200

    [joints2][JointsModel] Undo previous commit.

--- src/joints2/JointsModel.java
+++ src/joints2/JointsModel.java
@@ -1,6 +1,5 @@
 package joints2;
 
-import static java.lang.Math.abs;
 import static java.lang.Math.random;
 import static java.util.stream.Collectors.toList;
 import static java.util.stream.Collectors.toMap;
@@ -177,7 +176,7 @@ public final class JointsModel implements Serializable {
 				final double constraint = this.evaluateConstraint(segment);
 				double distance = point1.distance(point2);
 				
-				if (constraint * 0.1 < abs(distance - constraint)) {
+				if (distance != constraint) {
 					final Point3f middle = JointsEditorPanel.middle(point1, point2);
 					
 					if (distance == 0.0) {
